d1323bcc948697942a0b02e62173062b859bb8ed
Lorenzo Asquini <62559492+lorenzo-asquini@users.noreply.github.com>
2026-07-26 23:09:22 +0200

    Prettier

--- components/ui/NoWrap.tsx
+++ components/ui/NoWrap.tsx
@@ -3,7 +3,8 @@ import type { ReactNode } from 'react';
 export function NoWrap({ children }: { children: ReactNode }) {
     return (
         <>
-            {' '}<span className="whitespace-nowrap">{children}</span>{' '}
+            {' '}
+            <span className="whitespace-nowrap">{children}</span>{' '}
         </>
     );
 }
